333e4867bbded9775917d13731ddc8b61a37b3d0
sharon12307 <sharon12307@gmail.com>
2025-01-03 15:16:14 +0800

    0103-2

--- src/components/Disc.jsx
+++ src/components/Disc.jsx
@@ -1,15 +1,41 @@
+import { useRef, useState } from "react"
+
 function Disc({ coord, index, song, activeDiscIndex }) {
+    const audioRef = useRef(null);
+
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const handlePlay = () => {
+        audioRef.current.play();
+        setIsPlaying(true);
+    }
+    const handlePause = () => {
+        audioRef.current.pause();
+        setIsPlaying(false);
+    }
+    const togglePlay = () => {
+        if (isPlaying) {
+            handlePause();
+        } else {
+            handlePlay();
+        }
+
+    }
+
     return (
         <div className={`disc ${index === activeDiscIndex ? 'active' : ''}`}
-        style={{
-            left: `${coord.x}px`,
-            top: `${coord.y}px`,
-        }}>
+            style={{
+                left: `${coord.x}px`,
+                top: `${coord.y}px`,
+            }}>
             <img className="black-circle center-dot" src="./images/disc-circle.svg" alt="" />
             <img className="playlist center-dot" src={song.img} alt={song.song} />
             <img className="line center-dot" src="./images/disc-line.svg" alt="" />
             <div className="disc-center center-dot"></div>
-            <button className="play-btn center-dot"><img src="./images/btn-play.svg" alt="" /></button>
+            <button onClick={togglePlay}
+            className={`center-dot ${isPlaying ? 'pause-btn' : 'play-btn'}`}></button>
+            <audio ref={audioRef} src={song.audio}></audio>
+            {console.log(song.audio)}
         </div>
     )
 }
